8d75f28215bc7ac0397f65bc209490cbff507ebb
unknown <sivaranjanib99@gmail.com>
2021-08-05 19:44:24 +0530

    [siva] creating  the product review management

---
+++ ProjectReviewManagement/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProjectReviewManagement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Product Review Management");
+        }
+    }
+}
